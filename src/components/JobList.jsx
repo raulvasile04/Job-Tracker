233ea66f1a62bb3d38ccac036b80644be2475b6a
Raul-Valentin Vasile <raulvasile04@gmail.com>
2026-04-27 11:00:15 +0200

    Changed app colors

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -3,7 +3,6 @@ import JobCard from './JobCard';
 function JobList({jobs , deleteJob}) {
     return(
         <div>
-            <h2>Job Applications</h2>
             <div className="board">
             {jobs.map(job => (
                 <JobCard key={job.id} job={job} deleteJob={deleteJob}/>
